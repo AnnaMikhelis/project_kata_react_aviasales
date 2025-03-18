ee40393b22d757b0000bc415a6fac2611a1dc4ae
Anna Korovina <annmikhelis@gmail.com>
2025-03-18 19:47:27 +0500

    logo public

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Header.module.scss';
-import logo from '../../Logo.svg';
+import logo from '../../../public/Logo.svg';
 const Header = () => {
   return (
     <div className={styles.Header}>
